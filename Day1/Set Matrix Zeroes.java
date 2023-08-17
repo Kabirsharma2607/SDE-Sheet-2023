192d9b4f7c2af562889f2f9e7f60d87a9eaa2da4
Kabirsharma2607 <kabirsharma2607@gmail.com>
2023-08-17 22:35:05 +0530

    Update Set Matrix Zeroes.java

--- Day1/Set Matrix Zeroes.java
+++ Day1/Set Matrix Zeroes.java
@@ -1,20 +1,19 @@
- boolean hasFirstRow = false;
-        boolean hasFirstCol = false;
-        
-        for(int col = 0; col < matrix[0].length;col++){//First row
-            if(matrix[0][col]==0){
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean hasFirstRow = false, hasFirstCol = false;
+        for(int col = 0; col < matrix[0].length; col++){
+            if(matrix[0][col] == 0) {
                 hasFirstRow = true;
                 break;
             }
         }
-
-        for(int row = 0; row < matrix.length;row++){ // First Column
-            if(matrix[row][0]==0){
+        for(int row = 0; row < matrix.length; row++){
+            if(matrix[row][0] == 0){
                 hasFirstCol = true;
                 break;
             }
-        }   
-        for(int row = 1; row < matrix.length; row++){ //Preprocessing
+        }
+        for(int row = 1; row < matrix.length; row++){
             for(int col = 1; col < matrix[0].length; col++){
                 if(matrix[row][col] == 0){
                     matrix[row][0] = 0;
@@ -22,24 +21,26 @@
                 }
             }
         }
-        for(int col = 1; col < matrix[0].length; col++)
-            if(matrix[0][col] == 0) nullifyCol(matrix,col);
-        
-        for(int row = 1; row < matrix.length; row++)
-            if(matrix[row][0] == 0) nullifyRow(matrix,row);
-        
-        if(hasFirstRow) nullifyRow(matrix, 0);
-        if(hasFirstCol) nullifyCol(matrix, 0);
+        for(int col = 1; col < matrix[0].length; col++){
+            if(matrix[0][col]  == 0)
+                nullifyCol(matrix,col);
+        }
+        for(int row = 1; row < matrix.length; row++){
+            if(matrix[row][0] == 0){
+                nullifyRow(matrix,row);
+            }
+        }
+        if(hasFirstRow) nullifyRow(matrix,0);
+        if(hasFirstCol) nullifyCol(matrix,0);
+
     }
     private void nullifyRow(int[][] matrix, int row){
-        for(int col = 0; col < matrix[0].length; col++){
+        for(int col = 0; col < matrix[0].length; col++)
             matrix[row][col] = 0;
-        }
     }
-
-    private void nullifyCol(int[][] matrix, int col){
-        for(int row = 0; row < matrix.length; row++){
+    private void nullifyCol(int [][]matrix, int col){
+        for(int row = 0; row < matrix.length; row++)
             matrix[row][col] = 0;
-        }
     }
 }
+
